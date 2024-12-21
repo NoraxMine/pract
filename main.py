9363601fc3896245094978ebf1103de4b9da6578
NoraxMine <NoraxMine@tutamail.com>
2024-12-21 03:31:36 +0300

    +

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ from new import Pascal as ps
 
 
 def main():
-    user_choes = input("In ")
-    if user_choes.lower() == "t" :
+    user_choes = input("Для вызова функции по нахождению НОК введите 1 \nЕсли вам нужно посмотреть перестановки в определённом списке введите 2 \nЕсли хотите помтроить треугольник паскаля введите любую другую клавишу ")
+    if user_choes.lower() == "1" :
         user_temp = input("")
         user_temp  = user_temp.split()
         a,b = int(user_temp[0]),int(user_temp[1])
         print("", ek.gcd(a, b))
-    elif user_choes.lower() == "k" :
+    elif user_choes.lower() == "2" :
          print("", ex.spis())   
     else:
-        p = ps.pascal(int(input()))
-        print(p)
+        print(ps.pascal(int(input())))
         main()
     
 
